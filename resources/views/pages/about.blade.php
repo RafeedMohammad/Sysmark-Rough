36a292173bd26a6c65991b932f198c59d1bb1911
Rafeed Mohammad <rafeedmohammad1107@gmail.com>
2020-12-09 00:16:22 +0600

    First Commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.app')
+
+@section('title')
+    about
+@endsection
+
+
+@section('content')
+
+    <h1>Welcome to the about page</h1>
+        
+@endsection
+
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title')
+    Services
+@endsection
+
+
+@section('content')
+
+        <h1>Welcome to the services page</h1>
+
+@endsection('content')    
+
